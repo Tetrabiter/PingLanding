fcc97ff73fc0f2625f9cecf6e0fe6c16956ed650
Tim-Elesh <timofei.yelivanov@gmail.com>
2024-12-27 22:14:05 +0500

    add angular icons and pasted text in components

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { featherFacebook , featherTwitter , featherInstagram} from '@ng-icons/feather-icons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgIcon],
+  viewProviders: [provideIcons({ featherFacebook , featherTwitter , featherInstagram})],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
